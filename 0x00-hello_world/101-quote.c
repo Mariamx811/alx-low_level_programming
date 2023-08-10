798cf9bf2b01dace3cb9bc7b89d64956ad0adb1a
mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-08-10 14:00:11 -0700

    Task finished

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	write(STDout_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
