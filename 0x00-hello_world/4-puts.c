f2716fef3d24dc5053e67367dec338dbf11ccb6e
mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-08-10 12:52:47 -0700

    Task finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/* printing a line */
+/**
+ * main - Entry point
+ *
+ * Description: 'program writes a statement using puts'
+ * @parameter: void
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
