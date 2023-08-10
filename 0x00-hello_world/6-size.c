3404da4f77bf865763e26dbe21f1f6e79c900b9f
mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-08-10 13:19:12 -0700

    Task finished

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Program writes sizes of various types'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int num;
+	long lnum;
+	long long llnum;
+	float fnum;
+
+	printf("Size of a char: %lu", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu", (unsigned long)sizeof(num));	
+	printf("Size of a long int: %lu", (unsigned long)sizeof(lnum));	
+	printf("Size of a long long int: %lu", (unsigned long)sizeof(llnum));		   printf("Size of a float: %lu", (unsigned long)sizeof(fnum));
+	return (0);
+}
