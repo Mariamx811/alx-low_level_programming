397bf0227aea46e8d01ac8f3ff08884668860f12
mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-08-11 18:07:53 -0700

    Task finished

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'program'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf(i);
+	printf('\n');
+	return (0);
+}
