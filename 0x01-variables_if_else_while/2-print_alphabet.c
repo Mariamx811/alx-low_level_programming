4012292f30d3e61cf102946c47bace63b13a072c
mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-08-11 17:36:45 -0700

    Task finished

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'ASCII'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for(int i = 97; i<= 122;i++)
+	{
+		putchar((char)i);
+	}	
+	retrun (0);
+}
